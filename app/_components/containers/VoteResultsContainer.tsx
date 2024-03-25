d1501c71bd04d5a66d75ce52871c8abb18fb4b27
macaroni10y <56068155+macaroni10y@users.noreply.github.com>
2024-03-25 20:24:10 +0900

    implement loading placeholder

--- app/_components/containers/VoteResultsContainer.tsx
+++ app/_components/containers/VoteResultsContainer.tsx
@@ -1,3 +1,4 @@
+import DummyOneVoteResult from "@/app/_components/uiparts/DummyOneVoteResult";
 import OneVoteResult from "@/app/_components/uiparts/OneVoteResult";
 import { average, mode, scrumDecision } from "@/app/_lib/voteResultCalculate";
 import { CountableVote, Vote, isCountableVote } from "@/app/_types/types";
@@ -6,19 +7,18 @@ interface Props {
 	participantVotes: Vote[];
 }
 
-/**
- * contain multiple vote results such as average, mode...
- * @param participantVotes
- * @constructor
- */
-const VoteResultsContainer = ({ participantVotes }: Props) => {
-	const availableVotes = participantVotes.filter((it): it is CountableVote =>
-		isCountableVote(it),
-	) as CountableVote[];
-
-	const voteCompleted = participantVotes.every((it) => "not yet" !== it);
+function getDiv(availableVotes: CountableVote[], voteCompleted: boolean) {
+	if (availableVotes.length === 0) {
+		return (
+			<>
+				<DummyOneVoteResult title="average" />
+				<DummyOneVoteResult title="mode" />
+				<DummyOneVoteResult title="scrum decision" />
+			</>
+		);
+	}
 	return (
-		<div className="flex justify-center w-full max-w-5xl">
+		<>
 			<OneVoteResult
 				title="average"
 				value={average(availableVotes).toString()}
@@ -34,6 +34,24 @@ const VoteResultsContainer = ({ participantVotes }: Props) => {
 				value={scrumDecision(availableVotes).toString()}
 				voteCompleted={voteCompleted}
 			/>
+		</>
+	);
+}
+
+/**
+ * contain multiple vote results such as average, mode...
+ * @param participantVotes
+ * @constructor
+ */
+const VoteResultsContainer = ({ participantVotes }: Props) => {
+	const availableVotes = participantVotes.filter((it): it is CountableVote =>
+		isCountableVote(it),
+	) as CountableVote[];
+
+	const voteCompleted = participantVotes.every((it) => "not yet" !== it);
+	return (
+		<div className="flex justify-center w-full max-w-5xl">
+			{getDiv(availableVotes, voteCompleted)}
 		</div>
 	);
 };
---
+++ app/_components/uiparts/DummyOneVoteResult.tsx
@@ -0,0 +1,21 @@
+import styles from "@/app/page.module.css";
+
+interface Props {
+	title: string;
+}
+
+/**
+ * dummy component for OneVoteResult
+ * @param voteResult
+ * @constructor
+ */
+const DummyOneVoteResult = (voteResult: Props) => (
+	<div className="flex flex-col justify-center items-center flex-1 m-1">
+		<div>{voteResult.title}</div>
+		<div
+			className={`bg-gray-400 h-8 w-8 rounded ${styles.animatePlaceholder}`}
+		/>
+	</div>
+);
+
+export default DummyOneVoteResult;
