001f4d0b45f2237549b7cb3e293df5a41e8e8fcc
macaroni10y <56068155+macaroni10y@users.noreply.github.com>
2024-05-27 23:59:58 +0900

    login, signup with supabase

--- app/(routes)/login/page.tsx
+++ app/(routes)/login/page.tsx
@@ -1,33 +1,14 @@
 "use client";
-import React, {useState} from "react";
-import {supabase} from "@/app/_lib/supabase";
+import { login, signup } from "@/app/(routes)/login/action";
+import ActionButton from "@/app/_components/uiparts/ActionButton";
 import Header from "@/app/_components/uiparts/Header";
-import TheButton from "@/app/_components/uiparts/TheButton";
+import React, { useState } from "react";
 
 const Page = () => {
-    const [email, setEmail] = useState<string>("");
-    const [password, setPassword] = useState<string>("");
-	const [handleName, setHandleName] = useState<string>("");
+	const [isLogin, setIsLogin] = useState(true);
 
-	const handleSignIn = async () => {
-		await supabase.auth.signInWithPassword({
-			email: email,
-			password: password,
-		});
-	};
-	const handleSignUp = async () => {
-		await supabase.auth.signUp({
-			email: email,
-			password: password,
-			options: {
-				data: {
-					handleName: handleName,
-				}
-			}
-		});
-	};
-
-
+	const inputStyle = "w-full text-xs h-10 bg-gray-200 appearance-none border-2 border-gray-200 rounded py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-pink-300";
+	const labelStyle = "m-1 text-xs text-left w-full";
 
 	return (
 		<>
@@ -35,14 +16,67 @@ const Page = () => {
 				<Header />
 			</div>
 			<div className="h-screen bg-pink-50 flex items-center justify-center flex-col">
-				<div className="bg-white rounded-xl w-2/3 max-w-sm h-2/5 flex flex-col justify-evenly shadow-xl items-center">
-					<div className="flex flex-col">
-						<input className="m-2" type="text" placeholder="nickname" onChange={event => setHandleName(event.target.value)}/>
-						<input className="m-2" type="text" placeholder="email" onChange={event => setEmail(event.target.value)}/>
-						<input className="m-2" type="password" placeholder="password" onChange={event => setPassword(event.target.value)}/>
-					</div>
-					<TheButton text="Sign In" onClick={handleSignIn}/>
-					<TheButton text="Sign Up" onClick={handleSignUp}/>
+				<div
+					className={`${
+						isLogin ? "h-2/5" : "h-1/2"
+					} bg-white rounded-xl w-2/3 max-w-sm flex flex-col justify-evenly shadow-xl items-center`}
+				>
+					<form className="flex flex-col w-10/12 items-center">
+						{!isLogin && (
+							<>
+								<label className={labelStyle} htmlFor="nickname">
+									nickname
+								</label>
+								<input
+									className={inputStyle}
+									type="text"
+									name="nickname"
+									placeholder="nickname"
+								/>
+							</>
+						)}
+						<label className={labelStyle} htmlFor="email">
+							email
+						</label>
+						<input
+							className={inputStyle}
+							type="email"
+							name="email"
+							placeholder="email"
+						/>
+						<label className={labelStyle} htmlFor="password">
+							password
+						</label>
+						<input
+							className={inputStyle}
+							type="password"
+							name="password"
+							placeholder="password"
+						/>
+						{isLogin ? (
+							<>
+								<ActionButton text="Sign In" formAction={login} />
+								<button
+									type="button"
+									className="text-blue-500"
+									onClick={() => setIsLogin(false)}
+								>
+									Not yet registered? Sign Up
+								</button>
+							</>
+						) : (
+							<>
+								<ActionButton text="Sign Up" formAction={signup} />
+								<button
+									type="button"
+									className="text-blue-500"
+									onClick={() => setIsLogin(true)}
+								>
+									Sign In
+								</button>
+							</>
+						)}
+					</form>
 				</div>
 			</div>
 		</>
