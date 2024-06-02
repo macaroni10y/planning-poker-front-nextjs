19806139b4abf1dfc9f79187e852110ebca0b61d
macaroni10y <56068155+macaroni10y@users.noreply.github.com>
2024-06-03 00:42:20 +0900

    prevent force redirect to login page when visiting signup page

--- middleware.ts
+++ middleware.ts
@@ -7,6 +7,6 @@ export async function middleware(request: NextRequest) {
 
 export const config = {
 	matcher: [
-		"/((?!login|_next/static|_next/image|favicon.ico|.*\\.(?:svg|png|jpg|jpeg|gif|webp)$).*)",
+		"/((?!login|signup|_next/static|_next/image|favicon.ico|.*\\.(?:svg|png|jpg|jpeg|gif|webp)$).*)",
 	],
 };
