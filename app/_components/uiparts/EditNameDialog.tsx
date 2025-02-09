368b05c74f123a643793b99e3319a8018dd16162
macaroni10y <56068155+macaroni10y@users.noreply.github.com>
2025-02-09 17:43:44 +0900

    replace with shadcn input

--- app/_components/uiparts/EditNameDialog.tsx
+++ app/_components/uiparts/EditNameDialog.tsx
@@ -1,4 +1,5 @@
 import { Button } from "@/app/_components/uiparts/Button";
+import { Input } from "@/app/_components/uiparts/input";
 import { Dialog, DialogContent } from "@mui/material";
 import React, { useState } from "react";
 import { BiCheck } from "react-icons/bi";
@@ -31,9 +32,9 @@ const EditNameDialog = (props: Props) => {
                     <IoCloseSharp />
                 </div>
                 <div className="flex justify-center items-center">
-                    <input
+                    <Input
                         maxLength={15}
-                        className="w-2/3 bg-gray-200 appearance-none border-2 border-gray-200 rounded py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-pink-300"
+                        className="w-2/3 py-2 px-4"
                         type="text"
                         placeholder="input name"
                         onChange={(event) =>
